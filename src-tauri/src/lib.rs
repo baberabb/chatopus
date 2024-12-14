f2d3a934382b0da1a2e3c79ef3eb0c6c1178f4e8
Baber <baber@hey.com>
2024-12-14 22:44:19 +0000

    add database (buggy)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,10 +5,17 @@ use crate::config::{
 };
 use parking_lot;
 use std::sync::Arc;
+use sqlx::sqlite::SqlitePoolOptions;
 use tauri_plugin_localhost;
 use tauri_plugin_log::{Target, TargetKind};
-use tauri_plugin_sql;
 use tauri_plugin_store::StoreBuilder;
+use std::fs::OpenOptions;
+use tauri::{AppHandle, Manager};
+use sqlx::Pool;
+use sqlx::Sqlite;
+
+type Db = Pool<Sqlite>;
+
 
 mod apimodels;
 mod chat;
@@ -16,6 +23,40 @@ mod config;
 mod daemon;
 mod migrations;
 mod routes;
+
+
+async fn setup_db(app: &AppHandle) -> Db {
+    let mut path = app
+        .path()
+        .app_data_dir()
+        .expect("could not get data_dir");
+    match std::fs::create_dir_all(path.clone()) {
+        Ok(_) => {}
+        Err(err) => {
+            panic!("error creating directory {}", err);
+        }
+    };
+    path.push("db.sqlite");
+    let result = OpenOptions::new().create_new(true).write(true).open(&path);
+    match result {
+        Ok(_) => println!("database file created"),
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::AlreadyExists => println!("database file already exists"),
+            _ => {
+                panic!("error creating databse file {}", err);
+            }
+        },
+    }
+    let db = SqlitePoolOptions::new()
+        .connect(path.to_str().unwrap())
+        .await
+        .unwrap();
+    db
+}
+
+struct AppState {
+    db: Db,
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -32,10 +73,10 @@ pub fn run() {
                 ])
                 .build(),
         )
-        .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_store::Builder::default().build())
         .manage(ChatHistory(Arc::new(parking_lot::Mutex::new(Vec::new()))))
         .manage(ConfigState(parking_lot::Mutex::new(AppConfig::default())))
+        .manage(AppState { db })
         .invoke_handler(tauri::generate_handler![
             process_message,
             get_chat_history,
