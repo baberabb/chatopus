d008ebea6971c8dbf5414d78297c3616a750b476
Baber <baber@hey.com>
2024-12-18 17:00:40 +0000

    added million

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import MillionLint from "@million/lint";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    MillionLint.vite({
+      enabled: true,
+    }),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  clearScreen: false, // prevent vite from obscuring rust errors
+  
   server: {
     port: 1420,
     strictPort: true,
@@ -26,13 +29,14 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell vite to ignore watching `src-tauri`
+      // tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
-    resolve: {
-      alias: {
-          "@": path.resolve(__dirname, "./src"),
-      },
-    }
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
